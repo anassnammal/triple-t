8c5b53f9b26eb744a503dce174974326b13f3e79
ans <anassnammal@gmail.com>
2024-10-31 10:55:17 +0100

    Refactor game board component to use game state object

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,19 @@
 import React, { useEffect, useState, memo } from "react";
 
 type CellVal = 'X' | 'O' | '';
-type Player = Extract<CellVal, 'X' | 'O'>
+type GameState = {
+  cells: CellVal[],
+  player: Extract<CellVal, 'X' | 'O'>
+}
 type handleMoveFunc = (cellNo: number) => void;
 
 const CELL_COUNT = 9;
 const STYLES = {
-  board: "grid grid-cols-[auto_auto_auto]",
+  board: "relative grid grid-cols-[auto_auto_auto]",
   cell: "inline-flex justify-center items-center size-[calc(50vw/3)] border text-4xl",
 }
+
+const BOARD_ARR = Array(CELL_COUNT).fill('');
 
 const Cell = memo(
   ({ cellNo, val, handleMove }: { cellNo: number, val: CellVal, handleMove: handleMoveFunc }) => {
@@ -20,23 +25,35 @@ const Cell = memo(
 )
 
 const Board: React.FC = () => {
-  const [gameState, setGameState] = useState({ cells: Array(CELL_COUNT).fill(''), player: 'X' });
+  const [gameState, setGameState] = useState<GameState>({
+    cells: BOARD_ARR,
+    player: 'X'
+  });
 
   const handleMove: handleMoveFunc = (cellNo) => {
     if (gameState.cells[cellNo] !== '') return;
-  
+
     setGameState((prevState) => {
       const newCells = [...prevState.cells];
       newCells[cellNo] = prevState.player;
       const nextPlayer = prevState.player === 'X' ? 'O' : 'X';
+      if (!newCells.filter(c => c === '').length) {
+        console.log("match null")
+        return { cells: BOARD_ARR, player: nextPlayer };
+      }
       return { cells: newCells, player: nextPlayer };
     });
   };
-  
+
   return (
-      <div className={STYLES.board}>
-        {gameState.cells.map((cell: CellVal, i: number) => <Cell key={i} cellNo={i} val={cell} handleMove={handleMove}/>)}
-      </div>
+    <div className={STYLES.board}>
+      <span className="absolute flex items-center justify-center bg-green-600 h-[10vw] w-[30vw] left-[50%] translate-x-[-50%] border-b rounded-b-2xl text-4xl" >
+        0 - 0
+      </span>
+      {gameState.cells.map((cell: CellVal, i: number) => (
+        <Cell key={i} cellNo={i} val={cell} handleMove={handleMove} />
+      ))}
+    </div>
   )
 }
 
